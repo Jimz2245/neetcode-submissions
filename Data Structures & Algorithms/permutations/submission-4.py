3da840af029977aac431e5392e1e207e4dbfdee3
Jimz2245 <144721957+Jimz2245@users.noreply.github.com>
2026-06-19 17:51:09 -0400

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.py
@@ -0,0 +1,46 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        curr = []
+
+        def backtrack():
+            if len(curr) == len(nums):
+                res.append(curr.copy())
+                return
+            for num in nums:
+                if num not in curr:
+                    curr.append(num)
+                    backtrack()
+                    curr.pop()
+        
+        backtrack()
+        return res
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
