62dafde74711da4fdbc88d8a321d87babd397c08
Jimz2245 <144721957+Jimz2245@users.noreply.github.com>
2026-07-01 09:18:23 -0400

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        seen = set()
+        res = 0
+
+        connected = {i:[] for i in range(n)}
+
+        for i, j in edges:
+            connected[i].append(j)
+            connected[j].append(i)
+
+        q = collections.deque()
+
+        for i in range(n):
+            if i in seen:
+                continue
+            else:
+                q.append(i)
+                while q:
+                    node = q.popleft()
+                    for child in connected[node]:
+                        if child not in seen:
+                            q.append(child)
+                    seen.add(node)
+                res += 1
+
+        return res
+
+        
